7bc4c27f5f478c2adb907fe166496162986389c9
raj <60585242+rajbifrostglobal@users.noreply.github.com>
2021-04-26 18:49:16 +0200

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -114,9 +114,8 @@ function installTerraform {
 
 function installKubectl {
   echo "configuring kubectl"
-  curl -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
-  mkdir -p ~/.local/bin/kubectl
-  mv ./kubectl ~/.local/bin/kubectl
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  mv ./kubectl /usr/local/bin/kubectl
 }
 
 function main {
